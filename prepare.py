fe28e448cf4c309f9fb82200490a640d55216db9
rajeshlamichhane04 <107886574+rajeshlamichhane04@users.noreply.github.com>
2022-10-14 14:24:49 -0500

    functions cleaned up and added

--- prepare.py
+++ prepare.py
@@ -5,51 +5,71 @@ import nltk
 from nltk.corpus import stopwords
 import re
 
-def basic_clean(original):
+def basic_clean(string):
     #cast to lower case
-    article = original.lower()
+    string = string.lower()
     #remove accented and non Ascii characters
-    article = unicodedata.normalize("NFKD", article)\
+    string = unicodedata.normalize("NFKD", string)\
             .encode("ascii", "ignore")\
             .decode("utf-8")
     # remove special characters
-    article = re.sub(r'[^a-z0-9\'\s]',"", article)
+    string = re.sub(r'[^a-z0-9\s]',"", string)
     
-    return article
+    return string
 
 ##########################
 
-def tokenize(article):
+def tokenize(string):
     #create tokenizer
     tokenize = nltk.tokenize.ToktokTokenizer()
     #use tokenizer
-    article = tokenize.tokenize(article, return_str=True)
+    string = tokenize.tokenize(string, return_str=True)
     
-    return article
+    return string
 
  ############################
 
 
-def stem(article):
+def stem(string):
     #create porter stemmer
     ps = nltk.porter.PorterStemmer()
     #apply stemmer
     #this is going to give out a list
-    stems = [ps.stem(word) for word in article.split()]
+    stems = [ps.stem(word) for word in string.split()]
     #join the list back together
-    article_stemmed = " ".join(stems)
+    string = " ".join(stems)
     
-    return article_stemmed
+    return string
 
 ###################################################
 
-def lemmatize(article):
+def lemmatize(string):
     #create lemmatizer
     wnl = nltk.stem.WordNetLemmatizer()
     #use lemmatizer
     #splits back a list of words
-    lemmas = [wnl.lemmatize(word) for word in article.split()]
+    lemmas = [wnl.lemmatize(word) for word in string.split()]
     #join word back together
-    article_lemmatized = " ".join(lemmas)
+    string = " ".join(lemmas)
     
-    return article_lemmatized
+    return string
+    
+
+###############################################
+
+
+def remove_stopwords(string, extra_words = [], exclude_words = []):
+    # define stopword
+    stopwords_list = stopwords.words("english")
+    # exclude words you do not want to remove
+    stopwords_list = set(stopwords_list)-set(exclude_words)
+    # include extra words manauly in stop word list
+    stopwords_list = stopwords_list.union(set(extra_words))
+    #split lemmaztised paragraph 
+    words = string.split() 
+    #give me everything that is not stopword
+    filtered_words = [word for word in words if word not in stopwords_list]
+    #join filtered words
+    string = " ".join(filtered_words)
+    
+    return string
