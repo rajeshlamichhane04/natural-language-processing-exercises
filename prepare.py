1290118baf5ad8cc977764af6030b92881708a6f
rajeshlamichhane04 <107886574+rajeshlamichhane04@users.noreply.github.com>
2022-10-19 22:04:54 -0500

    added functions

--- prepare.py
+++ prepare.py
@@ -4,6 +4,8 @@ import unicodedata
 import nltk
 from nltk.corpus import stopwords
 import re
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 def basic_clean(string):
     #cast to lower case
@@ -73,3 +75,67 @@ def remove_stopwords(string, extra_words = [], exclude_words = []):
     string = " ".join(filtered_words)
     
     return string
+
+#################################################################
+
+
+def prepare_news_df(df):
+    #change column names
+    df.columns = ["title", "category", "original"]
+    df = df[["category","original"]]
+    #create new column that uses basic clean and tokenoze function
+    df["clean"] = df.original.apply(basic_clean).apply(tokenize)
+    #create new column that uses clean column and uses stem funtion 
+    df["stemmed"] = df.clean.apply(stem)
+    #create new column that takes in clean and applies lemmatize function
+    df["lemmatized"] = df.clean.apply(lemmatize)
+    df["cleaned_up"] = df.lemmatized.apply(remove_stopwords)
+    return df
+
+
+###########################################################################
+
+def prepare_blog_df(df):
+    #create new column that uses basic clean and tokenoze function
+    df["clean"] = df.content.apply(basic_clean).apply(tokenize)
+    #create new column that uses clean column and uses stem funtion 
+    df["stemmed"] = df.clean.apply(stem)
+    #create new column that takes in clean and applies lemmatize function
+    df["lemmatized"] = df.clean.apply(lemmatize)
+    df["cleaned_up"] = df.lemmatized.apply(remove_stopwords)
+    return df
+
+########################################################################
+
+#Define a function to automate plotting bigrams
+def plot_bigrams(words):
+    
+    word_data = {k[0] + ' ' + k[1]: v for k, v in words.to_dict().items()}
+    
+    word_img = WordCloud(background_color='white', width=800, height=600).generate_from_frequencies(word_data)
+    
+    plt.figure(figsize=(10, 6))
+    
+    plt.imshow(word_img)
+    
+    plt.axis('off')
+    
+    plt.show()
+    
+    
+##########################################################################
+
+#Create a function to automate plotting trigram
+def plot_trigrams(words):
+    
+    word_data = {k[0] + ' ' + k[1] + ' ' + k[2]: v for k, v in words.to_dict().items()}
+    
+    word_img = WordCloud(background_color='white', width=800, height=600).generate_from_frequencies(word_data)
+    
+    plt.figure(figsize=(10, 6))
+    
+    plt.imshow(word_img)
+    
+    plt.axis('off')
+    
+    plt.show()
